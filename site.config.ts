255dc00b4fb74233f1d71265cd7fd2ea22f82f1a
lushuai19921227 <68547294+lushuai19921227@users.noreply.github.com>
2022-11-11 12:59:04 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3a5ea95300ab4ef5a1ce2c7c86f26131',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'lus',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'lushuai',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
